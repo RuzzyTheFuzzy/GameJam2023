0689fae24abc91e870807bc983252f0a1eaabce2
RuzzyTheFuzzy <62539683+RuzzyTheFuzzy@users.noreply.github.com>
2023-11-18 11:24:21 +0100

    Added rotation to player movement
    Cha Cha real smooth

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,27 +5,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     [SerializeField] private Camera playerCamera;
     [SerializeField] private float gridSize;
     [SerializeField] private float moveTime;
 
     private Vector3 newPosition;
     private Vector3 oldPosition;
+    private Quaternion newRotation;
+    private Quaternion oldRotation;
     private float lerpTimer;
+
+    private Vector3 cameraOffset;
 
     private void Start( )
     {
         lerpTimer = moveTime;
+        cameraOffset = playerCamera.transform.position - transform.position;
     }
 
     private void Update( )
     {
-        Debug.Log(lerpTimer);
-        
         if ( lerpTimer >= moveTime )
         {
             var position = transform.position;
+
             if ( Input.GetKeyDown( "w" ) )
             {
                 position.z += gridSize;
@@ -48,13 +51,18 @@ public class Player : MonoBehaviour
                 oldPosition = transform.position;
                 newPosition = position;
                 lerpTimer = 0;
-            }
 
+                oldRotation = transform.rotation;
+                newRotation = Quaternion.LookRotation( ( position - transform.position ).normalized );
+            }
         }
         else
         {
             lerpTimer += Time.deltaTime;
             transform.position = Vector3.Lerp( oldPosition, newPosition, lerpTimer / moveTime );
+            transform.rotation = Quaternion.Lerp( oldRotation, newRotation, lerpTimer / moveTime );
         }
+
+        playerCamera.transform.position = transform.position + cameraOffset;
     }
 }
